b1f0a57dce6efe478649b4aa688b99910c293dd7
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2021-05-11 04:28:52 +0900

    Create Extra-Long-Factorials.cpp

---
+++ Extra-Long-Factorials.cpp
@@ -0,0 +1,65 @@
+long long Factorial(long long n)
+{
+    if (n <= 1)
+        return 1;
+    else
+        return n * Factorial(n - 1);
+}
+
+void extraLongFactorials(int n)
+{
+    std::deque<int> dq;
+    dq.push_front(1);
+    
+    for (int i = 2; i <= n; ++i)
+    {
+        for (auto &a : dq) a *= i;
+        
+        int carry = 0;
+        int temp = 0;
+        
+        for (auto it = dq.rbegin(); it != dq.rend(); ++it)
+        {
+            temp = (*it + carry) % 10;
+            carry = (*it + carry) / 10;
+            *it = temp;
+        }
+        
+        while (carry)
+        {
+            dq.push_front(carry % 10);
+            carry /= 10;
+        }
+    }
+        
+    for (auto &a : dq) cout << a;
+}
+
+void extraLongFactorials2(int n)
+{
+    vector<int> d;
+    d.push_back(1);
+    
+    for (int i = 2; i <= n; ++i)
+     {
+        for (auto it = d.begin(); it != d.end(); ++it)
+            *it *= i;
+        
+        for (size_t j = 0; j < d.size(); ++j)
+        {
+            if (d[j] < 10)
+                continue;
+
+            if (j == d.size()-1)
+            {
+                d.push_back(0);
+            }
+            
+            d[j+1] += d[j] / 10;
+            d[j] %= 10;
+        }
+    }
+
+    for (auto it = d.rbegin(); it != d.rend(); ++it)
+        cout << *it;    
+}
